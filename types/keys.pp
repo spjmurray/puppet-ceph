72f488e842769e103b33df02a4c74d23c59927ad
Nick Erdmann <n.erdmann@itc.rwth-aachen.de>
2017-07-10 10:29:04 +0200

    mgr key support

--- types/keys.pp
+++ types/keys.pp
@@ -4,4 +4,5 @@ type Ceph::Keys = Hash[String[1], Struct[{
   Optional[caps_mon] => String[1],
   Optional[caps_osd] => String[1],
   Optional[caps_mds] => String[1],
+  Optional[caps_mgr] => String[1],
 }]]
