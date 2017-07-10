2a89ed8d9de590a24c75f2edc7d8da5fa5a052b2
Simon Murray <simon.murray@datacentred.co.uk>
2017-07-10 13:28:19 +0100

    End of Life Ubuntu 14.04
    Testing seems to suggest this is intrinsically broken as regards udev, upstart,
    ceph-disk.  This removes all upstart related configuration - yay.

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,16 +6,10 @@ class ceph::params {
 
   case $::facts['os']['name'] {
     'Ubuntu': {
-      if versioncmp($::facts['os']['release']['full'], '16.04') >= 0 {
-        $service_provider = 'systemd'
-      } else {
-        $service_provider = 'upstart'
-      }
       $radosgw_package = 'radosgw'
       $prerequisites = []
     }
     'RedHat', 'Centos': {
-      $service_provider = 'systemd'
       $radosgw_package = 'ceph-radosgw'
       $prerequisites = [
         'redhat-lsb-core',            # Broken on centos with 0.94.6
--- manifests/service.pp
+++ manifests/service.pp
@@ -4,18 +4,16 @@
 #
 class ceph::service {
 
-  if $::ceph::service_provider == 'systemd' {
-    exec { 'ceph.target enable':
-      command => '/bin/systemctl enable ceph.target',
-      unless  => '/bin/systemctl is-enabled ceph.target',
-    } ~>
+  exec { 'ceph.target enable':
+    command => '/bin/systemctl enable ceph.target',
+    unless  => '/bin/systemctl is-enabled ceph.target',
+  } ~>
 
-    # Oddly I've seen OSD udev rules not applying on Xenial which are
-    # fixed with a reload
-    exec { 'ceph::service systemctl reload':
-      command     => '/bin/systemctl daemon-reload',
-      refreshonly => true,
-    }
+  # Oddly I've seen OSD udev rules not applying on Xenial which are
+  # fixed with a reload
+  exec { 'ceph::service systemctl reload':
+    command     => '/bin/systemctl daemon-reload',
+    refreshonly => true,
   }
 
 }
