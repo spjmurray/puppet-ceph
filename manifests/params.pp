2860cdc6a12ca3ca66a43e64c459fb0c39051ed7
Tom Downes <tpdownes@users.noreply.github.com>
2017-09-11 09:13:52 -0500

    Add support for Debian 8 and above (#9)

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,12 +4,12 @@
 #
 class ceph::params {
 
-  case $::facts['os']['name'] {
-    'Ubuntu': {
+  case $::facts['os']['family'] {
+    'Debian': {
       $radosgw_package = 'radosgw'
       $prerequisites = []
     }
-    'RedHat', 'Centos': {
+    'RedHat': {
       $radosgw_package = 'ceph-radosgw'
       $prerequisites = [
         'redhat-lsb-core',            # Broken on centos with 0.94.6
